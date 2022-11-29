8dd71237f52b97a146f69ffd43d7eee2b91f5839
ChaCha3088 <cha3088@gmail.com>
2022-11-29 15:40:43 +0900

    feat(/src/main/java/CarGameController.java): add 몇 번의 이동을 할 것인지를 입력받는다.

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,4 +4,8 @@ public class InputView {
     public static String[] inputCarNames() {
         return camp.nextstep.edu.missionutils.Console.readLine().split(",");
     }
+
+    public static int inputMoveCounts() {
+        return Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
+    }
 }
