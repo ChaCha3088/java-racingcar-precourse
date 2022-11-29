03ecc78a5fcaa5b5e3c4933165b3138a1099e6c3
ChaCha3088 <cha3088@gmail.com>
2022-11-29 17:12:56 +0900

    feat(/src/main/java/OutputView.java): add 차수별 실행 결과를 출력한다.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -9,7 +9,15 @@ public class Car {
     }
 
     // 추가 기능 구현
-    private void move() {
-        position ++;
+    public void move() {
+        this.position ++;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
     }
 }
--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public static void string(String str) {
         System.out.println(str);
     }
+
+    public static void result(Car car) {
+        System.out.println(car.getName()+" : "+"-".repeat(car.getPosition()));
+    }
 }
