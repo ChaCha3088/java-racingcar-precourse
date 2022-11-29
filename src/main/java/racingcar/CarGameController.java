a5990153fa186fc5ef17f04aed7957293be3188a
ChaCha3088 <cha3088@gmail.com>
2022-11-29 15:23:43 +0900

    feat(/src/main/java/CarGameController.java): add 이름이 5자 이하인지 검증한다.

--- src/main/java/racingcar/CarGameController.java
+++ src/main/java/racingcar/CarGameController.java
@@ -1,5 +1,8 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarGameController {
     private int generateRandomNumber() {
         return camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0, 9);
@@ -10,5 +13,13 @@ public class CarGameController {
             return true;
         }
         return false;
+    }
+
+    private void verifyLetterCounts(String[] inputLetters, int counts) {
+        for (String str: inputLetters) {
+            if (str.length() > counts || str.length() == 0) {
+                throw new IllegalArgumentException("[ERROR] 이름은 5자 이하거나 1자 이상이어야합니다.");
+            }
+        }
     }
 }
