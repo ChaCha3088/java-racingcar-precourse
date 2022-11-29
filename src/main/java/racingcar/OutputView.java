96c4592bb8953f1fa396f64aea31c71791ac5e60
ChaCha3088 <cha3088@gmail.com>
2022-11-29 17:40:30 +0900

    feat(/src/main/java/OutputView.java): add 최종 우승자를 출력한다.

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.List;
+
 public class OutputView {
     public static void string(String str) {
         System.out.println(str);
@@ -7,5 +9,9 @@ public class OutputView {
 
     public static void result(Car car) {
         System.out.println(car.getName()+" : "+"-".repeat(car.getPosition()));
+    }
+
+    public static void finalResult(List<String> list) {
+        System.out.println("최종 우승자 : "+String.join(",", list));
     }
 }
