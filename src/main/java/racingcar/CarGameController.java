3665ba73a1310184046f53d50603e11470ad4c54
ChaCha3088 <cha3088@gmail.com>
2022-11-29 16:20:55 +0900

    feat(/src/main/java/CarGameController.java): add 경주할 자동차 이름을 입력하라는 문구를 출력한다.

--- src/main/java/racingcar/CarGameController.java
+++ src/main/java/racingcar/CarGameController.java
@@ -34,6 +34,7 @@ public class CarGameController {
     private String[] repeatInputCarNames(String[] inputCarNames) {
         while (true) {
             try {
+                OutputView.string("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
                 return verifyLetterCounts(InputView.inputCarNames(), 5);
             } catch (IllegalArgumentException e) {
                 OutputView.string("[ERROR] 이름은 5자 이하거나 1자 이상이어야합니다.");
