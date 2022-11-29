0e997612a8053e1569b2a4a5dd45f4435cdb3928
ChaCha3088 <cha3088@gmail.com>
2022-11-29 14:45:42 +0900

    feat(/src/main/java/CarGameController.java): add 무작위 값이 4 이상일 경우 true를 return한다.

--- src/main/java/racingcar/CarGameController.java
+++ src/main/java/racingcar/CarGameController.java
@@ -4,4 +4,11 @@ public class CarGameController {
     private int generateRandomNumber() {
         return camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0, 9);
     }
+
+    private Boolean decideMovement(int randomNumber) {
+        if (randomNumber>= 4 & randomNumber<=9) {
+            return true;
+        }
+        return false;
+    }
 }
